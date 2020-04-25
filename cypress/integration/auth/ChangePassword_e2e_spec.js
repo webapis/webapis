f3a0c8d606586539fbfc26e6c192c8aca5507d4c
webapis <webapis.github@gmail.com>
2020-04-25 11:51:22 +0500

    v

--- cypress/integration/auth/ChangePassword_e2e_spec.js
+++ cypress/integration/auth/ChangePassword_e2e_spec.js
@@ -1,7 +1,6 @@
 import validationMessages from '../../../client/form/validationMessages';
 
-
-describe('Change password with token', () => {
+describe('CHANGE PASSWORD', () => {
   beforeEach(() => {
     cy.task('seed:login', {
       email: 'test@gmail.com',
@@ -10,130 +9,154 @@ describe('Change password with token', () => {
     });
   });
 
-  describe('Logged in user',()=>{
-    beforeEach(()=>{
+  describe('Change password with token', () => {
+    beforeEach(() => {
       cy.visit('http://localhost:3000/');
       cy.get('[data-testid=login]').click();
       cy.get('[data-testid=emailOrUsername]')
-        .type('tkm.house@gmail.com')
+        .type('test@gmail.com')
         .get('[data-testid=password]')
-        .type('DragondFFFly!2324.')
+        .type('Dragonfly1922!!')
         .get('[data-testid=login-btn]')
         .click();
-    })
-    it.only('confirm did not match to new password', () => {
-  
-  
-      cy.get('[data-testid=emailOrUsername]').type('test@gmail.com');
-      cy.get('[data-testid=password]').type('Dragonfly1922!!');
-      cy.get('[data-testid=login-btn]').click();
-      cy.wait(1000);
-      cy.get('[data-testid=change-password]').click();
-      cy.get('[data-testid=password]').type('Dragonfly1933!!');
-      cy.get('[data-testid=confirm]').type('Dragonfly1933!');
+      cy.get('[data-testid=changepassword]').click();
+    });
+    it('user sent empty password and confirm 406', () => {
+      cy.get('[data-testid=change-pass-btn]').click();
+      cy.get('[data-testid=message-password]').contains(
+        validationMessages.INVALID_PASSWORD
+      );
+      cy.get('[data-testid=message-confirm]').contains(
+        validationMessages.PASSWORDS_DO_NOT_MATCH
+      );
+    });
+
+    it('user sent invalid password server 406', () => {
+      cy.get('[data-testid=password]').type('123');
+      cy.get('[data-testid=confirm]').type('123');
+      cy.get('[data-testid=change-pass-btn]').click();
+      cy.get('[data-testid=message-password]').contains(
+        validationMessages.INVALID_PASSWORD
+      );
+    });
+
+    it('password did not match 412', () => {
+      cy.get('[data-testid=password]').type('Dragondlt197__d');
+      cy.get('[data-testid=confirm]').type('123');
       cy.get('[data-testid=change-pass-btn]').click();
       cy.get('[data-testid=message-confirm]').contains(
         validationMessages.PASSWORDS_DO_NOT_MATCH
       );
     });
-  
-  })
 
-  it('confirm did not match to new password', () => {
-    cy.visit('http://localhost:3000/');
+    it('Password changes successfully', () => {
+      cy.server();
+      cy.route('PUT', '/auth/changepass').as('changepass');
+      cy.get('[data-testid=password]').type('Dragondlt197_!dsds');
+      cy.get('[data-testid=confirm]').type('Dragondlt197_!dsds');
+      cy.get('[data-testid=change-pass-btn]').click();
 
-    cy.get('[data-testid=emailOrUsername]').type('test@gmail.com');
-    cy.get('[data-testid=password]').type('Dragonfly1922!!');
-    cy.get('[data-testid=login-btn]').click();
-    cy.wait(1000);
-    cy.get('[data-testid=change-password]').click();
-    cy.get('[data-testid=password]').type('Dragonfly1933!!');
-    cy.get('[data-testid=confirm]').type('Dragonfly1933!');
-    cy.get('[data-testid=change-pass-btn]').click();
-    cy.get('[data-testid=message-confirm]').contains(
-      validationMessages.PASSWORDS_DO_NOT_MATCH
-    );
+      cy.wait('@changepass').should((xhr) => {
+        expect(xhr.status).to.equal(200);
+      });
+    });
   });
-
-  it('Success ChangePassword with token', () => {
-    cy.visit('http://localhost:3000/auth/login');
-    cy.get('[data-testid=emailOrUsername]').type('test@gmail.com');
-    cy.get('[data-testid=password]').type('Dragonfly1922!!');
-    cy.get('[data-testid=login-btn]').click();
-    cy.wait(1000);
-    cy.get('[data-testid=change-password]').click();
-    cy.get('[data-testid=password]').type('Dragonfly1933!!');
-    cy.get('[data-testid=confirm]').type('Dragonfly1933!!');
-    cy.get('[data-testid=change-pass-btn]').click();
-    
-  });
-});
-
-describe('Change password with credentials', () => {
-  describe('Invalid credentials', () => {
+  describe('Change password with emailorusername and password', () => {
     beforeEach(() => {
-      cy.task('seed:delete', {});
-      cy.visit('http://localhost:3000/auth/signup');
-      cy.get('[data-testid=username]').type('lionardo');
-      cy.get('[data-testid=email]').type('lionardo@gmail.com');
-      cy.get('[data-testid=password]')
-        .type('Dragonfly1978.')
-        .get('[data-testid=signup-btn]')
-        .click();
-      cy.wait(1000);
+      cy.visit('http://localhost:3000');
+      cy.get('[data-testid=changepassword]').click();
     });
-    it('email not registered', () => {
-      cy.visit('http://localhost:3000/auth/changepassword');
-      cy.get('[data-testid=emailorusername]').type('tkmhouse@gmail.com');
+    it('user sent all fields empty ', () => {
+      cy.get('[data-testid=change-pass-btn]').click();
+      cy.get('[data-testid=message-emailorusername]').contains(
+        validationMessages.INVALID_USERNAME_OR_EMAIL
+      );
+      cy.get('[data-testid=message-current]').contains(
+        validationMessages.INVALID_EMPTY_STRING
+      );
+      cy.get('[data-testid=message-password]').contains(
+        validationMessages.INVALID_PASSWORD
+      );
+      cy.get('[data-testid=message-confirm]').contains(
+        validationMessages.PASSWORDS_DO_NOT_MATCH
+      );
+    });
+    it('user sent invalid emailorusername', () => {
+      cy.get('[data-testid=emailorusername]').type('123');
       cy.get('[data-testid=current]').type('Dragonfly1922!!');
-      cy.get('[data-testid=password]').type('Dragonfly1988_!');
-      cy.get('[data-testid=confirm]').type('Dragonfly1988_!');
+      cy.get('[data-testid=password]').type('Dragonfly1922!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1922!!');
       cy.get('[data-testid=change-pass-btn]').click();
-      cy.get('[data-testid=message-emailorusername]').contains(
-        validationMessages.INVALID_CREDENTIALS
-      );
-      cy.get('[data-testid=message-current]').contains(
-        validationMessages.INVALID_CREDENTIALS
-      );
-    });
-    it('username not registered', () => {
-      cy.visit('http://localhost:3000/auth/changepassword');
-      cy.get('[data-testid=emailorusername]').type('tkmhouse');
-      cy.get('[data-testid=current]').type('Dragonfly1978.');
-      cy.get('[data-testid=password]').type('Dragonfly1988_!');
-      cy.get('[data-testid=confirm]').type('Dragonfly1988_!');
-      cy.get('[data-testid=change-pass-btn]').click();
-      cy.get('[data-testid=message-emailorusername]').contains(
-        validationMessages.INVALID_CREDENTIALS
-      );
-      cy.get('[data-testid=message-current]').contains(
-        validationMessages.INVALID_CREDENTIALS
-      );
-    });
 
-    it('wrong password', () => {
-      cy.visit('http://localhost:3000/auth/changepassword');
-      cy.get('[data-testid=emailorusername]').type('lionardo@gmail.com');
+      cy.get('[data-testid=message-emailorusername]').contains(
+        validationMessages.INVALID_USERNAME_OR_EMAIL
+      );
+    });
+    it('user sent invalid password (username)', () => {
+      cy.get('[data-testid=emailorusername]').type('tkmhousenew');
       cy.get('[data-testid=current]').type('Dragonfly1922!');
-      cy.get('[data-testid=password]').type('Dragonfly1988_!');
-      cy.get('[data-testid=confirm]').type('Dragonfly1988_!');
+      cy.get('[data-testid=password]').type('Dragonfly1922!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1922!!');
       cy.get('[data-testid=change-pass-btn]').click();
 
+      cy.get('[data-testid=message-current]').contains(
+        validationMessages.INVALID_CREDENTIALS
+      );
+      cy.get('[data-testid=message-emailorusername]').contains(
+        validationMessages.INVALID_CREDENTIALS
+      );
+    });
+    it('user sent invalid password (email)', () => {
+      cy.get('[data-testid=emailorusername]').type('test@gmail.com');
+      cy.get('[data-testid=current]').type('Dragonfly1922!');
+      cy.get('[data-testid=password]').type('Dragonfly1922!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1922!!');
+      cy.get('[data-testid=change-pass-btn]').click();
+
+      cy.get('[data-testid=message-current]').contains(
+        validationMessages.INVALID_CREDENTIALS
+      );
+      cy.get('[data-testid=message-emailorusername]').contains(
+        validationMessages.INVALID_CREDENTIALS
+      );
+    });
+    it('confirm mismatch', () => {
+      cy.get('[data-testid=emailorusername]').type('test@gmail.com');
+      cy.get('[data-testid=current]').type('Dragonfly1922!!');
+      cy.get('[data-testid=password]').type('Dragonfly1922!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1922!');
+      cy.get('[data-testid=change-pass-btn]').click();
+      cy.get('[data-testid=message-confirm]').contains(
+        validationMessages.PASSWORDS_DO_NOT_MATCH
+      );
+    });
+
+    it('passwordChanged successfully', () => {
+      cy.server();
+      cy.route('PUT', '/auth/changepass').as('changepass');
+      cy.get('[data-testid=emailorusername]').type('test@gmail.com');
+      cy.get('[data-testid=current]').type('Dragonfly1922!!');
+      cy.get('[data-testid=password]').type('Dragonfly1923!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1923!!');
+      cy.get('[data-testid=change-pass-btn]').click();
+      cy.wait('@changepass').should((xhr) => {
+        expect(xhr.status).to.equal(200);
+      });
+    });
+
+    it('emailorusername does not exist',()=>{
+      cy.get('[data-testid=emailorusername]').type('tests@gmail.com');
+      cy.get('[data-testid=current]').type('Dragonfly1922!!');
+      cy.get('[data-testid=password]').type('Dragonfly1922!!');
+      cy.get('[data-testid=confirm]').type('Dragonfly1922!!');
+      cy.get('[data-testid=change-pass-btn]').click();
       cy.get('[data-testid=message-emailorusername]').contains(
         validationMessages.INVALID_CREDENTIALS
       );
       cy.get('[data-testid=message-current]').contains(
         validationMessages.INVALID_CREDENTIALS
       );
-    });
+    })
 
-    it('successful password change', () => {
-      cy.visit('http://localhost:3000/auth/changepassword');
-      cy.get('[data-testid=emailorusername]').type('lionardo@gmail.com');
-      cy.get('[data-testid=current]').type('Dragonfly1978.');
-      cy.get('[data-testid=password]').type('Dragonfly1988_!');
-      cy.get('[data-testid=confirm]').type('Dragonfly1988_!');
-      cy.get('[data-testid=change-pass-btn]').click();
-    });
   });
 });
--- server/auth/changePassword.js
+++ server/auth/changePassword.js
@@ -9,52 +9,17 @@ export default async function changePassword({ req, res, collection }) {
 
   try {
     const { emailorusername, password, current, confirm, token } = req.body;
-  debugger;
+    debugger;
     let user = null;
     let resBcrypt = null;
+    let errors = [];
     debugger;
     if (token) {
+      //----CHANGE PASSWORD WITH TOKEN----------------------------
       debugger;
-      const decoded = await jwt.verify(token, process.env.secret);
-      debugger;
-      let { id } = decoded;
-      debugger;
-      const salt = await bcrypt.genSalt(10);
-      debugger;
-      const hash = await bcrypt.hash(password, salt);
-      debugger;
-      const result = await collection.findOneAndUpdate(
-        { _id: new ObjectID(id) },
-        { $set: { password: hash } }
-      );
-      debugger;
-      user = result.value;
-      debugger;
-      const payload = { id, name: user.email };
-      debugger;
-      const newToken = jwt.sign(payload, process.env.secret, {
-        expiresIn: '300d',
-      });
-      debugger;
-      res.statusCode = 200;
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify({ token: newToken }));
-      res.end();
-    } else {
-      debugger;
-      let errors = [];
+      //-START -INITIAL INPUT CONSTRAINT TEST
 
-      //user sent empty email or username 410 tested----------------------------
-      if (validations.isEmptyEmailOrUsername({ emailorusername })) {
-        debugger;
-        errors.push(httpStatus.emailorusernameNotValid);
-      }
-      // user sent empty password 409 tested -----------------------------------
-      if (validations.isEmptyPassword({ password: current })) {
-        debugger;
-        errors.push(httpStatus.emptyStringNotValid);
-      }
-      // user sent empty password 409 tested -----------------------------------
+      // user sent empty OR invalid password 406  -----------------------------------
       debugger;
       if (
         validations.isEmptyPassword({ password }) ||
@@ -63,7 +28,7 @@ export default async function changePassword({ req, res, collection }) {
         debugger;
         errors.push(httpStatus.passwordInvalid);
       }
-      // user sent empty password 409 tested ----------------------------------
+      //user sent empty OR mismached confirm 412 tested ----------------------------------
       if (
         validations.isEmptyPassword({ password: confirm }) ||
         !validations.passwordsMatch({ password, confirm })
@@ -71,26 +36,111 @@ export default async function changePassword({ req, res, collection }) {
         debugger;
         errors.push(httpStatus.passwordDoNotMatch);
       }
-
+      //-- END ----INITIAL INPUT CONSTRAINT TEST
       if (errors.length > 0) {
+        //INITIAL INPUT CONSTRAINT TEST NOT VALID
         debugger;
         res.statusCode = 400;
         res.writeHead(400, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify({ errors }));
         res.end();
       } else {
-        //valid email
+        //INITIAL INPUT CONSTRAINT TEST  VALID
+        //UPDATE PASSORD
+        const decoded = await jwt.verify(token, process.env.secret);
+        debugger;
+        let { id } = decoded;
+        debugger;
+        const salt = await bcrypt.genSalt(10);
+        debugger;
+        const hash = await bcrypt.hash(password, salt);
+        debugger;
+        const result = await collection.findOneAndUpdate(
+          { _id: new ObjectID(id) },
+          { $set: { password: hash } }
+        );
+        debugger;
+        user = result.value;
+        debugger;
+        const payload = { id, name: user.email };
+        debugger;
+        const newToken = jwt.sign(payload, process.env.secret, {
+          expiresIn: '300d',
+        });
+        debugger;
+        res.statusCode = 200;
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ token: newToken }));
+        res.end();
+      }
+    } else {
+      //---------CHANGE PASSWORD WITH EMAILORUSERNAME AND PASSWORD----------------!
+      debugger;
+
+      //-START -INITIAL INPUT CONSTRAINT TEST
+
+      //UNAUTHENTICATED USER: user sent empty emailorusername 410 ----------------------------
+      if (validations.isEmptyEmailOrUsername({ emailorusername })) {
+        debugger;
+        errors.push(httpStatus.emailorusernameNotValid);
+      }
+      //UNAUTHENTICATED USER: user sent not empty value but invalid email or username ----------------------------
+      if (!validations.isEmptyEmailOrUsername({ emailorusername })) {
+        if (
+          !validations.isValidEmail({ email: emailorusername }) &&
+          !validations.isValidUsername({ username: emailorusername })
+        ) {
+          errors.push(httpStatus.emailorusernameNotValid);
+        }
+      }
+      //UNAUTHENTICATED USER:  user sent empty current 409  -----------------------------------
+      if (validations.isEmptyPassword({ password: current })) {
+        debugger;
+        errors.push(httpStatus.emptyStringNotValid);
+      }
+      //UNAUTHENTICATED AND AUTHENTICATED USER:  user sent empty OR invalid password 406  ----------------------------------
+      debugger;
+      if (
+        validations.isEmptyPassword({ password }) ||
+        !validations.isValidPasspword({ password })
+      ) {
+        debugger;
+        errors.push(httpStatus.passwordInvalid);
+      }
+      // UNAUTHENTICATED AND AUTHENTICATED USER: user sent empty OR mismached confirm 412 tested ----------------------------------
+      if (
+        validations.isEmptyPassword({ password: confirm }) ||
+        !validations.passwordsMatch({ password, confirm })
+      ) {
+        debugger;
+        errors.push(httpStatus.passwordDoNotMatch);
+      }
+      //-- END ----INITIAL INPUT CONSTRAINT TEST
+      if (errors.length > 0) {
+        //INITIAL INPUT CONSTRAINT TEST NOT VALID
+        debugger;
+        res.statusCode = 400;
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ errors }));
+        res.end();
+      } else {
+        //INITIAL INPUT CONSTRAINT TEST VALID
+        // START- VALIDATION TEST
         if (validations.isValidEmail({ email: emailorusername })) {
+          //USER SENT EMAIL
           debugger;
           user = await collection.findOne({ email: emailorusername });
           debugger;
           if (user) {
+            //EMAIL EXIST
             debugger;
             resBcrypt = await bcrypt.compare(current, user.password);
             debugger;
             if (resBcrypt) {
+              // EMAIL EXISTS BUT CURRENT PASS IS INVALID
               debugger;
               updatePassword({
+                //UPDATE PASSWORD
                 emailorusername: 'email',
                 value: emailorusername,
                 password,
@@ -98,8 +148,9 @@ export default async function changePassword({ req, res, collection }) {
                 res,
               });
             } else {
+              //EMAIL EXISTS BUT PASSWORD IS INVALID
               debugger;
-              //invalid credentials
+              //invalid credentials------------401
               errors.push(httpStatus.credentialInvalid);
               res.statusCode = 400;
               res.writeHead(400, { 'Content-Type': 'application/json' });
@@ -107,8 +158,9 @@ export default async function changePassword({ req, res, collection }) {
               res.end();
             }
           } else {
+            //EMAIL IS VALID BUT DOES NOT EXIST
             debugger;
-            //email does not exist
+            //email does not exist----------------401
             errors.push(httpStatus.credentialInvalid);
             res.statusCode = 400;
             res.writeHead(400, { 'Content-Type': 'application/json' });
@@ -116,17 +168,21 @@ export default async function changePassword({ req, res, collection }) {
             res.end();
           }
         } else {
+          //USER SENT USERNAME
           //valid username-------------------------------------------
           debugger;
           user = await collection.findOne({ username: emailorusername });
           debugger;
           if (user) {
+            // USERNAME EXISTS
             debugger;
-            resBcrypt = await bcrypt.compare(password, user.password);
+            resBcrypt = await bcrypt.compare(current, user.password);
             debugger;
             if (resBcrypt) {
+              //CURRENT PASSWORD MATCHES
               debugger;
               updatePassword({
+                //UPDATE PASSWORD
                 emailorusername: 'username',
                 value: emailorusername,
                 password,
@@ -134,8 +190,9 @@ export default async function changePassword({ req, res, collection }) {
                 res,
               });
             } else {
+              //USERNAME EXISTS BUT CURRENT PASSWORD DOES NOT MATCH
               debugger;
-              //invalid credentials
+              //invalid credentials----------401
               errors.push(httpStatus.credentialInvalid);
               res.statusCode = 400;
               res.writeHead(400, { 'Content-Type': 'application/json' });
@@ -143,8 +200,9 @@ export default async function changePassword({ req, res, collection }) {
               res.end();
             }
           } else {
+            //VALID USERNAME BUT DOES NOT EXIST
             debugger;
-            //username does not exist
+            //username does not exist ----------401
             errors.push(httpStatus.credentialInvalid);
             res.statusCode = 400;
             res.writeHead(400, { 'Content-Type': 'application/json' });
