edc17da44b9de79f137074b36867acc332ad354e
webapis <webapis.github@gmail.com>
2020-04-29 15:27:25 +0500

    v

---
+++ client/auth/change-password/change-password.js
@@ -0,0 +1,4 @@
+import 'whatwg-fetch';
+import { h, render } from 'preact';
+
+render(<div>Change Password</div>,  document.body)
--- rollup.config.js
+++ rollup.config.js
@@ -3,53 +3,87 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import image from '@rollup/plugin-image';
-//import image from 'rollup-plugin-image-files';
 import serve from 'rollup-plugin-serve';
 import htmlTemplate from 'rollup-plugin-generate-html-template';
 import del from 'rollup-plugin-delete';
 import { terser } from 'rollup-plugin-terser';
 import replace from 'rollup-plugin-replace';
 const production = !process.env.ROLLUP_WATCH;
-
-export default {
-  input: `client/${process.env.appName}/index.js`,
-  output: [
-    {
-      dir: `apps/${process.env.appName}/build`,
-      format: 'es',
-      sourcemap: 'inline',
-    },
-  ],
-  plugins: [
-    del({ targets: `apps/${process.env.appName}/build/*` }),
-    htmlTemplate({
-      template: 'config/rollup/html-template/index.html',
-      target: `apps/${process.env.appName}/build/index.html`,
-      attrs: ['type="module"'],
-    }),
-    image(),
-    postcss({
-      extensions: ['.css'],
-      plugins: [],
-    }),
-    resolve(),
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      plugins: [
-        [
-          '@babel/plugin-transform-react-jsx',
-          {
-            pragma: 'h',
-            pragmaFrag: 'Fragment',
-          },
-        ],
+//  ,
+const commonPlugins = [
+  image(),
+  postcss({
+    extensions: ['.css'],
+    plugins: [],
+  }),
+  resolve(),
+  babel({
+    babelrc: false,
+    exclude: 'node_modules/**',
+    plugins: [
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          pragma: 'h',
+          pragmaFrag: 'Fragment',
+        },
       ],
-    }),
-    production && terser(),
-    replace({
-      api_url: JSON.stringify(process.env.api_url),
-    }),
-    serve(`apps/${process.env.appName}/build/`),
-  ],
-};
+    ],
+  }),
+  production && terser(),
+  replace({
+    api_url: JSON.stringify(process.env.api_url),
+  }),
+];
+
+export default [
+  {
+    input: `client/${process.env.appName}/index.js`,
+    output: [
+      {
+        dir: `apps/${process.env.appName}/build`,
+        format: 'es',
+        sourcemap: 'inline',
+      },
+    ],
+    plugins: [
+      del({ targets: `apps/${process.env.appName}/build/*` }),
+      ...commonPlugins,
+      htmlTemplate({
+        template: 'config/rollup/html-template/index.html',
+        target: `apps/${process.env.appName}/build/index.html`,
+        attrs: ['type="module"'],
+      }),
+      serve({
+        contentBase: `apps/${process.env.appName}/build/`,
+        openPage: '/index.html',
+        port: 10001,
+        open: true,
+      }),
+    ],
+  },
+  {
+    input: `client/auth/change-password/change-password.js`,
+    output: [
+      {
+        dir: `apps/${process.env.appName}/build`,
+        format: 'es',
+        sourcemap: 'inline',
+      },
+    ],
+    plugins: [
+      ...commonPlugins,
+      htmlTemplate({
+        template: 'config/rollup/html-template/changepassword.html',
+        target: `apps/${process.env.appName}/build/changepassword.html`,
+        attrs: ['type="module"'],
+      }),
+      serve({
+        contentBase: `apps/${process.env.appName}/build/`,
+        openPage: '/changepassword.html',
+        port: 10002,
+        open: true,
+      }),
+    ],
+  },
+];
