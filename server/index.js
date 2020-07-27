8850595cc07e7774dfbc22078759e0055bf3acd7
webapis <webapis.github@gmail.com>
2020-07-27 16:21:41 +0500

    onwait3

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 //import http from 'http';
 const ws = require("./wsocket");
 //import { parseServer } from "./parse";
