d9619d5a1c104498d4cef0870f8d3858b1e97872
webapis <webapis.github@gmail.com>
2020-07-27 17:15:43 +0500

    ffff

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { MongoClient } = require("mongodb");
 // parse
 if (process.env.PREACT_APP_BACK === "PREACT_APP_PARSE") {
   // parseServer();
-}
+} //
 
 const options = {
   key: fs.readFileSync("key.pem"),
