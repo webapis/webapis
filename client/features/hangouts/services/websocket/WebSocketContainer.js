670bc9bfa3bc11bd3aa3cff036dd2168fd2ccb59
webapis <webapis.github@gmail.com>
2020-08-03 09:50:33 +0500

    v

--- client/features/hangouts/services/websocket/WebSocketContainer.js
+++ client/features/hangouts/services/websocket/WebSocketContainer.js
@@ -64,7 +64,7 @@ export function WebSocketContainer(props) {
   }, [searchHangouts]);
 
   useEffect(() => {
-    if (pendingHangout && socket.readyState === 1) {
+    if (pendingHangout && socket && socket.readyState === 1) {
       sendPendingHangout();
     }
   }, [pendingHangout, socket]);
