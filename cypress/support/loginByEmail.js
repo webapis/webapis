d4e9313ec3ea1f067fc48f428812322a9b5a4d04
webapis <webapis.github@gmail.com>
2020-08-03 09:19:59 +0500

    c

--- cypress/support/loginByEmail.js
+++ cypress/support/loginByEmail.js
@@ -1,4 +1,5 @@
 Cypress.Commands.add("loginByEmail", ({ email, password }) => {
+  cy.log("email and password", email, password);
   cy.request({
     url: "https://localhost:3000/auth/login",
     method: "GET",
@@ -7,6 +8,7 @@ Cypress.Commands.add("loginByEmail", ({ email, password }) => {
       "Access-Control-Allow-Headers": "*",
       Authorization: `Basic ${btoa(`${email}:${password}`)}`,
     },
+    failOnStatusCode: false,
   })
     .its("body")
     .then((body) => {
