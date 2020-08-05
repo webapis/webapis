033a2264d638051c8153da1dcafe426902957266
webapis <webapis.github@gmail.com>
2020-08-05 11:02:22 +0500

    https fixed

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ const options = {
   await client.connect();
   console.log("process.env.NODE_ENV", process.env.NODE_ENV);
   if (process.env.NODE_ENV === "production") {
-    server = http.createServer(httpRoute(client));
+    server = https.createServer(options, httpRoute(client));
+    //  server = http.createServer(httpRoute(client));
   } else {
     server = https.createServer(options, httpRoute(client));
   }
