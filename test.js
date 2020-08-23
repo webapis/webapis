02938ec8a8d75e3d7cee5ea64965ecb55294a452
webapis <webapis.github@gmail.com>
2020-08-23 09:09:35 -0700

    v

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 const test = require("ava");
+const withPage = require("./test/_withPage");
+const url = "https://localhost:3000";
+// test("foo", (t) => {
+//   t.pass();
+// });
 
-test("foo", (t) => {
-  t.pass();
-});
-
-test("bar", async (t) => {
-  const bar = Promise.resolve("bar");
-  t.is(await bar, "bar");
-});
+// test("bar", async (t) => {
+//   const bar = Promise.resolve("bar");
+//   t.is(await bar, "bar");
+// });
+test(
+  'page title should contain "Google"',
+  withPage,
+  async (t, berosPage, demosPage) => {
+    await berosPage.goto(url);
+    await demosPage.goto(url);
+    t.true((await berosPage.title()).includes("Document"));
+  }
+);
--- test/_withPage.js
+++ test/_withPage.js
@@ -1,12 +1,44 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 module.exports = async (t, run) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let BeroslaunchOptions = {
+    headless: false,
+    ignoreHTTPSErrors: true,
+    args: ["--window-position=0,0", "--window-size=300,700"],
+    executablePath: "Chrome/Application/chrome.exe", // because we are using puppeteer-core so we must define this option
+  };
+
+  let DemoslaunchOptions = {
+    headless: false,
+    ignoreHTTPSErrors: true,
+    args: ["--window-position=550,0", "--window-size=300,700"],
+    executablePath: "Chrome/Application/chrome.exe", // because we are using puppeteer-core so we must define this option
+  };
+
+  const berosBrowser = await puppeteer.launch(BeroslaunchOptions);
+  const berosPage = await berosBrowser.newPage();
+  await berosPage.setViewport({
+    width: 500,
+    height: 400,
+    isMobile: true,
+    deviceScaleFactor: 1,
+  });
+  const demosBrowser = await puppeteer.launch(DemoslaunchOptions);
+  const demosPage = await demosBrowser.newPage();
+  await demosPage.setViewport({
+    width: 500,
+    height: 400,
+    isMobile: true,
+    deviceScaleFactor: 1,
+  });
+  //const browser = await puppeteer.launch();
+  //const page = await browser.newPage();//
   try {
-    await run(t, page);
+    await run(t, berosPage, demosPage);
   } finally {
-    await page.close();
-    await browser.close();
+    await berosPage.close();
+    await demosPage.close();
+    await demosBrowser.close();
+    await berosBrowser.close();
   }
 };
