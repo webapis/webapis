3af086fb49e3612f802d1fb8d34be90653496a6e
webapis <webapis.github@gmail.com>
2020-08-25 17:22:04 +0500

    v

--- client/features/hangouts/ui-components/Hangouts.js
+++ client/features/hangouts/ui-components/Hangouts.js
@@ -1,17 +1,25 @@
 import { h } from "https://cdnjs.cloudflare.com/ajax/libs/preact/10.4.6/preact.module.js";
 import htm from "https://cdnjs.cloudflare.com/ajax/libs/htm/3.0.4/htm.module.js";
+import { useState } from "https://cdn.jsdelivr.net/gh/webapis/webapis@cdn/assets/libs/prod/hooks.cdn.js";
 const html = htm.bind(h);
 
 export default function Hangouts({
   hangouts,
-  onNavigation,
-  state,
-  dispatch,
-  onAppRoute,
-  username,
+  // onNavigation,
+  // state,
+  // dispatch,
+  // onAppRoute,
+  // username,
   search,
   searchResult = "",
-  guestEmail,
+  guestEmail, //InviteGuest
+  invitationSuccess, //InviteGuest
+  userNotFound, //InviteGuest
+  searching, //SeachComponent
+  invitingGuest, // Invite Guest
+  inviteGuest, // Invite Guest
+  messageForGuest, // InviteGuest
+  searchComplete, //SearchComponent
 }) {
   return html`
     <div class="container-fluid bg-success" style="height:90vh">
@@ -26,20 +34,33 @@ export default function Hangouts({
                 placeholder="Enter username or email"
                 btnTitle="Search"
                 search=${search}
+                searching=${searching}
+                searchComplete=${searchComplete}
               />
               ${searchResult === "notfound" &&
-              html` <${InviteGuest} email=${guestEmail} /> `}
+              html`
+                <${InviteGuest}
+                  guestEmail=${guestEmail}
+                  invitationSuccess=${invitationSuccess}
+                  userNotFound=${userNotFound}
+                  invitingGuest=${invitingGuest}
+                  inviteGuest=${inviteGuest}
+                  messageForGuest=${messageForGuest}
+                />
+              `}
 
               <div class="list-group">
                 ${hangouts &&
                 hangouts.map((h) => {
                   return html`
-                    <a
-                      href="#"
-                      class="list-group-item list-group-item-action mb-1 border rounded-pill border-success"
-                    >
-                      ${h.username}
-                    </a>
+                    <div>
+                      <a
+                        href="#"
+                        class="list-group-item list-group-item-action mb-1 border rounded-pill border-success"
+                      >
+                        ${h.username}
+                      </a>
+                    </div>
                   `;
                 })}
               </div>
@@ -51,7 +72,13 @@ export default function Hangouts({
   `;
 }
 
-function SeachComponent({ placeholder, btnTitle, search }) {
+function SeachComponent({
+  placeholder,
+  btnTitle,
+  search,
+  searching,
+  searchComplete,
+}) {
   return html`
     <div class="input-group mb-3">
       <input
@@ -61,15 +88,105 @@ function SeachComponent({ placeholder, btnTitle, search }) {
         value=${search}
       />
       <div class="input-group-append">
+        ${searchComplete &&
+        html`
+          <button
+            disabled=${search === ""}
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+          >
+            <${BackSpace} />
+          </button>
+        `}
+
         <button
           disabled=${search === ""}
           class="btn btn-outline-secondary"
           type="button"
           id="button-addon2"
         >
-          ${btnTitle}
+          ${searching
+            ? html` <span
+                class="spinner-border spinner-border-sm"
+                role="status"
+                aria-hidden="true"
+              ></span>`
+            : html` ${btnTitle}`}
         </button>
       </div>
+    </div>
+  `;
+}
+
+export function InviteGuest({
+  guestEmail,
+  guestusername = "user",
+  invitationSuccess,
+  userNotFound,
+  invitingGuest,
+  inviteGuest,
+  messageForGuest,
+}) {
+  const [state, setState] = useState({ invite: false });
+
+  return html`
+    <div>
+      <div>
+        ${userNotFound &&
+        html`
+          <div class="alert alert-warning" role="alert">
+            ${guestusername} is not found.
+            <button class="btn btn-link">
+              Invite as a guest.
+            </button>
+          </div>
+        `}
+        ${invitationSuccess &&
+        html`
+          <div class="alert alert-success" role="alert">
+            Guest invitation sent successfuly.
+            <button class="btn btn-outline-success small">
+              ok
+            </button>
+          </div>
+        `}
+      </div>
+      ${inviteGuest &&
+      html`
+        <textarea
+          disabled=${invitingGuest}
+          value=${messageForGuest}
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="2"
+        ></textarea>
+        <div class="input-group mb-3 mt-1">
+          <input
+            disabled=${invitingGuest}
+            type="email"
+            class="form-control"
+            placeholder="Enter guest's email"
+            value=${guestEmail}
+          />
+          <div class="input-group-append">
+            <button
+              disabled=${guestEmail === ""}
+              class="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+            >
+              ${invitingGuest
+                ? html` <span
+                    class="spinner-border spinner-border-sm"
+                    role="status"
+                    aria-hidden="true"
+                  ></span>`
+                : html` Invite`}
+            </button>
+          </div>
+        </div>
+      `}
     </div>
   `;
 }
@@ -92,30 +209,28 @@ export function PeopleIcon() {
   `;
 }
 
-export function InviteGuest({ email }) {
+export function BackSpace() {
   return html`
-    <div>
-      <small id="emailHelp" class="form-text text-danger"
-        >User is not registered. Invite as a guest</small
-      >
-      <div class="input-group mb-3">
-        <input
-          type="email"
-          class="form-control"
-          placeholder="Enter email"
-          value=${email}
-        />
-        <div class="input-group-append">
-          <button
-            disabled=${email === ""}
-            class="btn btn-outline-secondary"
-            type="button"
-            id="button-addon2"
-          >
-            Invite as a guest
-          </button>
-        </div>
-      </div>
-    </div>
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      class="bi bi-backspace"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M6.603 2h7.08a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-7.08a1 1 0 0 1-.76-.35L1 8l4.844-5.65A1 1 0 0 1 6.603 2zm7.08-1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08z"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M5.83 5.146a.5.5 0 0 0 0 .708l5 5a.5.5 0 0 0 .707-.708l-5-5a.5.5 0 0 0-.708 0z"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M11.537 5.146a.5.5 0 0 1 0 .708l-5 5a.5.5 0 0 1-.708-.708l5-5a.5.5 0 0 1 .707 0z"
+      />
+    </svg>
   `;
 }
--- storybook/hangout/hangouts.ui.state.js
+++ storybook/hangout/hangouts.ui.state.js
@@ -8,31 +8,55 @@ export default function HanogutUiState() {
     <div style="height:100%">
       <h5 class="bg-success text-white text-center">Initial State</h5>
       <${Hangouts}
-        hangouts=${[{ username: "demos" }, { username: "beros" }]}
+        hangouts=${[
+          { username: "demos" },
+          { username: "beros" },
+          { username: "guru" },
+        ]}
         search=""
       />
       <h5 class="bg-success text-white text-center">
         User Entered Search Input
       </h5>
-      <${Hangouts}
-        hangouts=${[{ username: "demos" }, { username: "beros" }]}
-        search="feros"
-      />
+      <${Hangouts} hangouts=${[]} search="feros" />
+      <h5 class="bg-success text-white text-center">
+        User Clicked Search Button
+      </h5>
+      <${Hangouts} hangouts=${[]} search="feros" searching=${true} />
       <h5 class="bg-success text-white text-center">User is not found</h5>
       <${Hangouts}
-        hangouts=${[{ username: "demos" }, { username: "beros" }]}
+        hangouts=${[]}
         searchResult="notfound"
         guestEmail=""
+        search="testUser"
+        userNotFound=${true}
+        searchComplete=${true}
       />
       <h5 class="bg-success text-white text-center">
-        Guest Invitation is send
+        User clicked invite button.
       </h5>
       <${Hangouts}
-        hangouts=${[{ username: "demos" }, { username: "beros" }]}
+        hangouts=${[]}
         searchResult="notfound"
+        invitingGuest=${true}
+        inviteGuest=${true}
+        guestEmail="test@gmail.com"
+        messageForGuest="Hello demo let's chat as a guest!"
+      />
+      <h5 class="bg-success text-white text-center">
+        Guest Invitation is send successfully
+      </h5>
+      <${Hangouts}
+        hangouts=${[]}
+        searchResult="notfound"
+        invitationSuccess=${true}
+        inviteGuest=${false}
       />
       <h5 class="bg-success text-white text-center">User is found</h5>
-      <${Hangouts} hangouts=${[{ username: "demos" }, { username: "beros" }]} />
+      <${Hangouts}
+        search="feros"
+        hangouts=${[{ username: "feros", state: "INVITEE" }]}
+      />
     </div>
   `;
 }
