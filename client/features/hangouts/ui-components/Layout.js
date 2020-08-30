1d26aad4fcbbb3bce678adbf508c62fcf8d336f0
webapis <webapis.github@gmail.com>
2020-08-30 09:32:03 -0700

    v

--- client/features/hangouts/ui-components/Layout.js
+++ client/features/hangouts/ui-components/Layout.js
@@ -1,15 +1,8 @@
 import { h } from "https://cdnjs.cloudflare.com/ajax/libs/preact/10.4.6/preact.module.js";
 import htm from "https://cdnjs.cloudflare.com/ajax/libs/htm/3.0.4/htm.module.js";
-import GearIcon from "icons/bootstrap/GearIcon";
-const styles = {
-  root: {
-    backgroundColor: "#eeeeee",
-    height: "100%",
-    position: "relative",
-  },
-};
+
 const html = htm.bind(h);
-export default function Layout({ children, style, id }) {
+export default function Layout({ children, id }) {
   return html`
     <div
       class="container-fluid bg-success"
