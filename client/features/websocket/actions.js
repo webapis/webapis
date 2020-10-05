384008758de0c9545e7e429c4bbfce1d53c5fef9
webapis <webapis.github@gmail.com>
2020-10-05 09:02:24 +0500

    v

--- client/features/websocket/actions.js
+++ client/features/websocket/actions.js
@@ -1,6 +1,6 @@
 import actionTypes from "./actionTypes";
 
 export function initWebSocket({ url, dispatch }) {
-  const websocket = new WebSocket(url);
+  const websocket = new WebSocket(url.replace(/^http/, "ws"));
   dispatch({ type: actionTypes.WEBSOCKET_INITIALIZED, websocket });
 }
