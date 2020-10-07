925b783559b3c3ad48abafdf356c2dd85b688053
webapis <webapis.github@gmail.com>
2020-10-07 15:13:43 +0500

    v

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -70,80 +70,72 @@ let demoslaunchOptions = {
     await demosPage.goto(url, { waitUntil: "networkidle2" });
     // //navbar-toggler
     //await berosPage.waitForNavigation({ waitUntil: "networkidle2" });
-    // await berosPage.waitForSelector(".navbar-toggler");
+    await berosPage.waitForSelector(".navbar-toggler");
 
-    // await berosPage.click(".navbar-toggler");
+    await berosPage.click(".navbar-toggler");
 
-    // await berosPage.waitForSelector("#signup");
+    await berosPage.waitForSelector("#signup");
 
-    // await berosPage.click("#signup");
+    await berosPage.click("#signup");
 
-    // await berosPage.click(".navbar-toggler");
-    // await berosPage.waitForSelector("#username");
+    await berosPage.waitForSelector("#username");
     // // debugger;
-    // await berosPage.type("#username", "berouser");
+    await berosPage.type("#username", "berouser");
 
-    // await berosPage.type("#email", "berouser@gmail.com");
-    // await berosPage.type("#password", "Testfly1977!");
-    // await berosPage.click("#signup-btn");
-    // //await t.context.demosPage.goto(url, { waitUntil: "networkidle2" });
+    await berosPage.type("#email", "berouser@gmail.com");
+    await berosPage.type("#password", "Testfly1977!");
+    await berosPage.click("#signup-btn");
+    await demosPage.goto(url, { waitUntil: "networkidle2" });
 
-    // await demosPage.click(".navbar-toggler");
-    // await demosPage.waitForSelector("#signup");
+    await demosPage.click(".navbar-toggler");
+    await demosPage.waitForSelector("#signup");
 
-    // await demosPage.click("#signup");
-    // await demosPage.click(".navbar-toggler");
-    // await demosPage.waitForSelector("#username");
-    // await demosPage.type("#username", "demouser");
-    // await demosPage.type("#email", "demouser@gmail.com");
-    // await demosPage.type("#password", "Testfly1977!");
-    // await demosPage.click("#signup-btn");
+    await demosPage.click("#signup");
 
-    // // // //navbar-toggler
-    // // await demosPage.waitForNavigation({ waitUntil: "networkidle2" });
+    await demosPage.waitForSelector("#username");
+    await demosPage.type("#username", "demouser");
+    await demosPage.type("#email", "demouser@gmail.com");
+    await demosPage.type("#password", "Testfly1977!");
+    await demosPage.click("#signup-btn");
+
     // // // //berouser send invitation
-    // await berosPage.waitForSelector("input[data-testid=user-search-input]");
-    // await berosPage.type("input[data-testid=user-search-input]", "demouser");
-    // await berosPage.click("button[data-testid=user-search-button]");
-    // await berosPage.waitForSelector("a[data-testid=demouser]");
-    // await berosPage.click("a[data-testid=demouser]");
+    await berosPage.waitForSelector("input[data-testid=user-search-input]");
+    await berosPage.type("input[data-testid=user-search-input]", "demouser");
+    await berosPage.click("button[data-testid=user-search-button]");
+    await berosPage.waitForSelector("a[data-testid=demouser]");
+    await berosPage.click("a[data-testid=demouser]");
 
-    // await berosPage.waitForSelector("div[data-testid=invite-ui]");
-    // await berosPage.click("button[data-testid=oninvite-btn]");
+    await berosPage.waitForSelector("div[data-testid=invite-ui]");
+    await berosPage.click("button[data-testid=oninvite-btn]");
 
     // // //demouser accepts invitation
-    // await demosPage.waitForFunction(
-    //   'document.querySelector("span[data-testid=message-count]").innerText.includes("1")'
-    // );
+    await demosPage.waitForFunction(
+      'document.querySelector("span[data-testid=message-count]").innerText.includes("1")'
+    );
 
-    // await demosPage.click("a[data-testid=unread-link]");
-    // await demosPage.waitForSelector("ul[data-testid=unread-ui]");
-    // await demosPage.waitForSelector("li[data-testid=berouser]");
-    // await demosPage.click("li[data-testid=berouser]");
-    // await demosPage.waitForSelector("div[data-testid=hangchat-ui]");
-    // await demosPage.click("button[data-testid=decline-btn]");
-    // await demosPage.waitForSelector("div[data-testid=hangchat-ui]");
-    // await demosPage.waitForSelector("div[data-testid=hangchat-ui]");
-    debugger;
-    // //berouser sees new unread message
-    // //berouser opens unread new message
-    // await berosPage.waitForFunction(
-    //   'document.querySelector("span[data-testid=message-count]").innerText.includes("1")'
-    // );
-    // await berosPage.click("a[data-testid=unread-link]");
-    // await berosPage.waitForSelector("ul[data-testid=unread-ui]");
-    // await berosPage.waitForSelector("li[data-testid=demouser]");
-    // await berosPage.click("li[data-testid=demouser]");
+    await demosPage.click("a[data-testid=unread-link]");
+    await demosPage.waitForSelector("ul[data-testid=unread-ui]");
+    await demosPage.waitForSelector("li[data-testid=berouser]");
+    await demosPage.click("li[data-testid=berouser]");
+    await demosPage.waitForSelector("div[data-testid=hangchat-ui]");
+    await demosPage.click("button[data-testid=accept-btn]");
+    await demosPage.waitForSelector("div[data-testid=hangchat-ui]");
 
-    //hangchat-ui visible
     //berouser types a message
     //berouser sends a message to demouser
-    // await berosPage.waitForSelector("div[data-testid=hangchat-ui]");
-    // await berosPage.type(
-    //   "input[data-testid=message-input]",
-    //   "Hello bero. how are you ?"
-    // );
-    // await berosPage.click("button[data-testid=send-btn]");
+    await berosPage.waitForSelector("div[data-testid=hangchat-ui]");
+    await berosPage.type(
+      "input[data-testid=message-input]",
+      "Hello bero. how are you ?"
+    );
+    await berosPage.click("button[data-testid=send-btn]");
+    //  await berosPage.waitForSelector("div[data-testid=hangchat-ui]");
+    await demosPage.type(
+      "input[data-testid=message-input]",
+      "I am fine thanks how are you?"
+    );
+    await demosPage.click("button[data-testid=send-btn]");
+    debugger;
   } catch (error) {
     console.log("error", error);
   } finally {
