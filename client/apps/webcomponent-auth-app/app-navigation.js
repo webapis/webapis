39783ccc1d65d8260e4ac351ee2655188276fd42
webapis <webapis.github@gmail.com>
2020-10-15 17:44:50 +0500

    v

--- client/apps/webcomponent-auth-app/app-navigation.js
+++ client/apps/webcomponent-auth-app/app-navigation.js
@@ -9,6 +9,7 @@ class AppNavigation extends HTMLElement {
     let signinBtn = this.shadowRoot.querySelector("#signin-btn");
     let signupBtn = this.shadowRoot.querySelector("#signup-btn");
     let signoutBtn = this.shadowRoot.querySelector("#signout-btn");
+    const shadowRoot = this.shadowRoot;
     signinBtn.addEventListener("click", (e) => {
       e.preventDefault();
       pubsub.publish("route", { route: "signin" });
@@ -19,12 +20,61 @@ class AppNavigation extends HTMLElement {
       pubsub.publish("route", { route: "signup" });
     });
 
-    signoutBtn.addEventListener("signout", (e) => {
+    signoutBtn.addEventListener("click", (e) => {
       e.preventDefault();
       pubsub.publish("route", { route: "signout" });
+      this._signout({ shadowRoot });
     });
+
+    pubsub.subscribe("auth_success", (data) => {
+      this._auth(data);
+    });
+
+    this._initialload({ shadowRoot });
+  } // connectCallback
+
+  _signout({ shadowRoot }) {
+    shadowRoot.querySelector("#signup-btn").setAttribute("hidden", false);
+    shadowRoot.querySelector("#signin-btn").setAttribute("hidden", false);
+    window.localStorage.removeItem("webcom");
   }
-}
+  _auth(data) {
+    const { token, username, email } = data;
+    window.localStorage.setItem(
+      "webcom",
+      JSON.stringify({
+        token,
+        username,
+        email,
+      })
+    );
+    shadowRoot.querySelector(
+      "#profile-btn"
+    ).textContent = `${username}, signed in`;
+    shadowRoot.querySelector("#signup-btn").classList.add("hide");
+    shadowRoot.querySelector("#signin-btn").setAttribute("hidden", true);
+    shadowRoot.querySelector("#profile-btn").setAttribute("hidden", false);
+    shadowRoot.querySelector("#signout-btn").setAttribute("hidden", false);
+  }
+  _initialload({ shadowRoot }) {
+    if (window.localStorage.getItem("webcom")) {
+      debugger;
+      const { token, username, email } = JSON.parse(
+        window.localStorage.getItem("webcom")
+      );
+      debugger;
+      pubsub.publish("local_auth", { token, email, username });
+      shadowRoot.querySelector(
+        "#profile-btn"
+      ).textContent = `${username}, signed in`;
+      shadowRoot.querySelector("#signup-btn").setAttribute("hidden", true);
+      shadowRoot.querySelector("#signin-btn").setAttribute("hidden", true);
+    } else {
+      shadowRoot.querySelector("#profile-btn").setAttribute("hidden", true);
+      shadowRoot.querySelector("#signout-btn").setAttribute("hidden", true);
+    }
+  }
+} //class
 customElements.define("app-navigation", AppNavigation);
 
 function createTemplate({ shadowRoot }) {
@@ -32,7 +82,11 @@ function createTemplate({ shadowRoot }) {
 
   template.innerHTML = `   <link rel="stylesheet" href="libs/bootstrap.css" />
   <!-- JS, Popper.js, and jQuery -->
-
+ <style> 
+   .hide {
+     display:none;
+   }
+ </style>
   <script src="libs/jquery.slim.min.js"></script>
 
   <script src="libs/bootstrap.js"></script>
@@ -54,12 +108,12 @@ function createTemplate({ shadowRoot }) {
         <ul class="navbar-nav mr-auto"></ul>
         <form class="form-inline my-2 my-lg-0">
           <button
-            id="profile"
+            id="profile-btn"
             data-testid="profile-link"
             class="btn  my-2 my-sm-0 btn-sm btn-outline-success mr-1"
             type="button"
           >
-            , signed in
+            
           </button>
 
           <button
